7e28f8be74a749c7c73fe55422122c8f30ad61ad
mahadiweb <64768414+mahadiweb@users.noreply.github.com>
2021-09-28 23:20:31 +0600

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,5 +1,5 @@
 <?php
-require("connection.php");
+require("include/connection.php");
 //delete ppp user
 $api->comm("/ppp/secret/remove", array(
 		".id" => "Enter user id which do you want to delete",
