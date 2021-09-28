30025b274eba1865664fb9711a0b8352436f7e5d
mahadiweb <64768414+mahadiweb@users.noreply.github.com>
2021-09-28 23:09:49 +0600

    Create update.php

---
+++ update.php
@@ -0,0 +1,12 @@
+<?php
+require("connection.php");
+//update ppp user
+$api->comm("/ppp/secret/set", array(
+      ".id"     => "Enter user id which do you want to update",
+      "name"     => "test",
+      "password" => "test",
+      "service"  => "pppoe",
+      "profile"  => "default",
+      "comment"  => "testing",
+   	));
+?>
