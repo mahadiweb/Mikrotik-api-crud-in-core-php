cbf0214c9e39d961fadbd80166d03bf7ffd9f717
mahadiweb <64768414+mahadiweb@users.noreply.github.com>
2021-09-28 23:15:02 +0600

    Create fetch single user info.php

---
+++ fetch single user info.php
@@ -0,0 +1,9 @@
+<?php
+require("connection.php");
+//fetch single ppp user data
+$id = "Enter user id which do you want to show";
+	$singleuser = $api->comm("/ppp/secret/print", array(
+    "?.id" => "$id",
+  	));
+  	print_r($singleuser);
+?>
