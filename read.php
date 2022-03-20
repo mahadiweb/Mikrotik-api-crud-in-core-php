a6f78d7d86f3beb1a2226f92b552560f95c2a407
Md.Mahadi Hasan <64768414+mahadiweb@users.noreply.github.com>
2022-03-21 01:02:49 +0600

    Update read.php

--- read.php
+++ read.php
@@ -1,6 +1,6 @@
 <?php
 require("include/connection.php");
 //read ppp user
-$read = $api->comm('/ppp/secret/print');
+$read = $api->comm('/ppp/secret/print');  //also use 'getall' in print 
 print_r($read);
 ?>
